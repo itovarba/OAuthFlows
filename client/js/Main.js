46bc36c3f1bbc62fd6278a180e96b0f68e098c71
itovarba <iosu.igor.tovar.barranco@everis.com>
2021-05-31 10:42:30 +0200

    fix on main.js for callbacks

--- client/js/Main.js
+++ client/js/Main.js
@@ -1,5 +1,5 @@
 var $apiEle = $("#apiCount"), 
-proxyURL = 'https://localhost:8081/proxy/'; 
+proxyURL = 'https://' + process.env.DOMAIN_NAME + '/proxy/' || 'https://localhost:8081/proxy/'; 
 
 var client = new forcetk.Client('', '', proxyURL);
  
